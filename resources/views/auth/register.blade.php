3e261abe074f5d4286bc09cc155b2c93cce34b39
JDkinior <jeliecerdelgado@ucundinamarca.edu.co>
2026-03-01 13:40:43 -0500

    Refactor registration form layout and update title in register view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,10 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Registro</title>
 </head>
 <body>
     <h1>Register</h1>
+    <form action="{{ route('register.store') }}" method="POST">
+        
+        @csrf  <label for="name">Nombre:</label>
+        <input type="text" name="name" id="name" required><br><br>
+
+        <label for="email">Correo:</label>
+        <input type="email" name="email" id="email" required><br><br>
+
+        <button type="submit">Registrarse</button>
+    </form>
 </body>
 </html>
