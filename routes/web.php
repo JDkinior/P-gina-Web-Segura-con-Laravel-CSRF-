2ca2cbec16685784db42ef3a910aa690be2855fa
JDkinior <jeliecerdelgado@ucundinamarca.edu.co>
2026-03-01 13:39:47 -0500

    Fix middleware syntax and correct route name for login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController; // Es buena práctica importarlo
 
 Route::get('/', function () {
     return view('inicio');
@@ -10,6 +11,5 @@ Route::get('/register', [UserController::class, 'create'])->name('register');
 Route::post('/register', [UserController::class, 'store'])->name('register.store');
 
 Route::get('/login', [UserController::class, 'create'])->name('login');
-Route::post('/login', [UserController::class, 'store'])->Route::middleware('throtle:5,1');
--> name('login.store');
-    
+// Sintaxis corregida para el middleware y el nombre de la ruta:
+Route::post('/login', [UserController::class, 'store'])->middleware('throttle:5,1')->name('login.store');
